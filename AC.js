4ebef0d733686435e1b83bc2fe26dc34938afbaf
Aaron Clinger <aaron@aaronclinger.com>
2020-03-28 12:43:30 -0400

    Initial commit of AC.js

---
+++ AC.js
@@ -0,0 +1,352 @@
+/**
+ * @author Aaron Clinger - https://github.com/aaronclinger/ac.js
+ */
+(function(document, window) {
+	'use strict';
+	
+	function ACElement(element) {
+		var pub = {};
+		
+		pub.getElement = function() {
+			return element;
+		};
+		
+		pub.on = function(eventName, eventHandler, options) {
+			element.addEventListener(eventName, eventHandler, options);
+			
+			return pub;
+		};
+		
+		pub.off = function(eventName, eventHandler) {
+			element.removeEventListener(eventName, eventHandler);
+			
+			return pub;
+		};
+		
+		pub.height = function() {
+			return element.offsetHeight;
+		};
+		
+		pub.width = function() {
+			return element.offsetWidth;
+		};
+		
+		pub.offset = function() {
+			var rect = element.getBoundingClientRect();
+			
+			return {
+				top: rect.top + window.AC.pageY(),
+				left: rect.left + window.AC.pageX()
+			};
+		};
+		
+		pub.style = function() {
+			return element.style;
+		};
+		
+		pub.data = function(id) {
+			return element.getAttribute('data-' + id);
+		};
+		
+		pub.get = function(selector, onlyFirst) {
+			return window.AC.get(selector, onlyFirst, element);
+		};
+		
+		pub.prepend = function(el) {
+			if (el.getElement) {
+				el = el.getElement();
+			}
+			
+			element.insertBefore(el, element.firstChild);
+			
+			return pub;
+		};
+		
+		pub.append = function(el) {
+			if (el.getElement) {
+				el = el.getElement();
+			}
+			
+			element.appendChild(el);
+			
+			return pub;
+		};
+		
+		pub.remove = function() {
+			element.parentNode.removeChild(element);
+			
+			return pub;
+		};
+		
+		pub.empty = function() {
+			pub.setHTML(null);
+			
+			return pub;
+		};
+		
+		pub.hasClass = function(className) {
+			return element.classList.contains(className);
+		};
+		
+		pub.addClass = function(className) {
+			element.classList.add(className);
+			
+			return pub;
+		};
+		
+		pub.removeClass = function(className) {
+			element.classList.remove(className);
+			
+			return pub;
+		};
+		
+		pub.toggleClass = function(className) {
+			if (pub.hasClass(className)) {
+				pub.removeClass(className);
+			} else {
+				pub.addClass(className);
+			}
+		};
+		
+		pub.setAttribute = function(name, value) {
+			element.setAttribute(name, value);
+			
+			return pub;
+		};
+		
+		pub.removeAttribute = function(name) {
+			element.removeAttribute(name);
+			
+			return pub;
+		};
+		
+		pub.getAttribute = function(name) {
+			return element.getAttribute(name);
+		};
+		
+		pub.setText = function(text) {
+			element.textContent = text;
+			
+			return pub;
+		};
+		
+		pub.getText = function() {
+			return element.textContent;
+		};
+		
+		pub.setHTML = function(html) {
+			element.innerHTML = html;
+			
+			return pub;
+		};
+		
+		pub.getHTML = function() {
+			return element.innerHTML;
+		};
+		
+		pub.getParent = function() {
+			return window.AC.wrap(element.parentNode);
+		};
+		
+		return pub;
+	}
+	
+	function AC() {
+		var pub = {};
+		
+		var init = function() {
+			pub.TRANSITION_EVENT      = whichTransitionEvent();
+			pub.ANIMATION_EVENT       = whichAnimationEvent();
+			pub.SUPPORTS_PASSIVE      = doesSupportPassive();
+			pub.PASSIVE_PARAM         = pub.SUPPORTS_PASSIVE ? { passive: true } : false;
+			pub.SUPPORTS_INTERSECTION = 'IntersectionObserver' in window;
+		};
+		
+		pub.wrap = function(element) {
+			return new ACElement(element);
+		};
+		
+		pub.ready = function(callback) {
+			if (document.readyState === 'interactive' || document.readyState === 'complete') {
+				callback();
+			} else {
+				document.addEventListener('DOMContentLoaded', callback);
+			}
+		};
+		
+		pub.get = function(selector, onlyFirst, scope) {
+			var selectorType = onlyFirst ? 'querySelector' : 'querySelectorAll',
+			    list,
+			    el,
+			    i;
+			
+			scope = scope || document;
+			
+			if (selector.indexOf('#') === 0 && selector.indexOf(' ') === selector.indexOf('.')) {
+				selectorType = 'getElementById';
+				selector     = selector.substr(1, selector.length);
+			}
+			
+			el = scope[selectorType](selector);
+			
+			if (el.length === 0) {
+				return null;
+			} else if (el.length === 1 && onlyFirst) {
+				return pub.wrap(el[0]);
+			} else if (el.length > 0) {
+				list = [];
+				i    = -1;
+				
+				while (++i < el.length) {
+					list.push(pub.wrap(el[i]));
+				}
+				
+				return list;
+			}
+			
+			return pub.wrap(el);
+		};
+		
+		pub.getRequest = function(url, callback, errorCallback) {
+			var request = new XMLHttpRequest();
+			
+			request.open('GET', url, true);
+			
+			request.onload = function() {
+				if (request.status >= 200 && request.status < 400) {
+					if (callback) {
+						callback(request.responseText);
+					}
+				} else {
+					if (errorCallback) {
+						errorCallback();
+					}
+				}
+			};
+			
+			request.onerror = errorCallback;
+			
+			request.send();
+			
+			return request;
+		};
+		
+		pub.new = function(elString) {
+			if (elString.indexOf('<') === 0) {
+				return pub.wrap(pub.new('div').setHTML(elString).getElement().firstChild);
+			}
+			
+			return pub.wrap(document.createElement(elString));
+		};
+		
+		pub.rateLimit = function(callback, limit) {
+			var wait  = false,
+			    queue = false;
+			
+			function delay() {
+				setTimeout(function() {
+					if (queue) {
+						callback.call();
+						
+						queue = false;
+						
+						delay();
+					} else {
+						wait = false;
+					}
+				}, limit);
+			}
+			
+			return function() {
+				if ( ! wait) {
+					callback.call();
+					
+					wait = true;
+					
+					delay();
+				} else {
+					queue = true;
+				}
+			};
+		};
+		
+		pub.each = function(list, callback) {
+			var i = -1,
+			    l = list.length;
+			
+			while (++i < l) {
+				if (callback(list[i], i) === false) {
+					return;
+				}
+			}
+		};
+		
+		pub.pageX = function() {
+			return (window.pageXOffset !== undefined) ? window.pageXOffset : (document.documentElement || document.body.parentNode || document.body).scrollLeft;
+		};
+		
+		pub.pageY = function() {
+			return (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+		};
+		
+		var whichTransitionEvent = function() {
+			var el = document.createElement('div'),
+			    t;
+			
+			var transitions = {
+				transition      : 'transitionend',
+				OTransition     : 'oTransitionEnd',
+				MozTransition   : 'transitionend',
+				WebkitTransition: 'webkitTransitionEnd'
+			};
+			
+			for (t in transitions) {
+				if (el.style[t] !== undefined) {
+					return transitions[t];
+				}
+			}
+		};
+		
+		var whichAnimationEvent = function() {
+			var el = document.createElement('div'),
+			    t;
+			
+			var animations = {
+				animation      : 'animationend',
+				OAnimation     : 'oAnimationEnd',
+				MozAnimation   : 'animationend',
+				WebkitAnimation: 'webkitAnimationEnd'
+			};
+			
+			for (t in animations) {
+				if (el.style[t] !== undefined) {
+					return animations[t];
+				}
+			}
+		};
+		
+		var doesSupportPassive = function() {
+			var supportsPassive = false;
+			
+			try {
+				var opts = Object.defineProperty({}, 'passive', {
+					get: function() {
+						supportsPassive = true;
+					}
+				});
+				
+				window.addEventListener('test', null, opts);
+			} catch (e) {
+				return false;
+			}
+			
+			return supportsPassive;
+		};
+		
+		init();
+		
+		return pub;
+	}
+	
+	window.AC = AC();
+}(document, window));
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# ac.js
+# AC.js
+
+`AC.js` is a simple utility designed to streamline common tasks in JavaScript. It’s built to be a simple tool when a larger JavaScript library or framework is not needed.
+
+## Documentation
+
+While `AC.js` does not provided documentation, it’s a very simple utility and can be learned very quickly. Here is an example of some of its usage:
+
+```js
+AC.ready(function() {
+	var limit = AC.rateLimit(function() {
+		console.log('Will be called once every 500ms when scrolling');
+	}, 1000);
+	
+	AC.wrap(window).on('scroll', limit, AC.PASSIVE_PARAM); 	
+});
+```
+
+## License
+
+`AC.js` can be used freely for any open source or commercial works and is released under a [MIT license](http://en.wikipedia.org/wiki/MIT_License).
+
+
+## Author
+
+[Aaron Clinger](http://aaronclinger.com)
